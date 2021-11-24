110afec65b51125f7dde87b46f29d3e15c4aff04
Jacek Kopecky <jacek@jacek.cz>
2021-11-24 23:26:51 +0000

    stage 5

--- stages/server/index.js
+++ stages/server/index.js
@@ -9,6 +9,11 @@ app.get('/auth-config', (req, res) => {
   res.json(authConfig);
 });
 
+// a simple API route that will greet anyone
+app.get('/api/hello', (req, res) => {
+  res.send(`Hello! The time is ${new Date()}`);
+});
+
 // this will serve the files present in static/ inside this stage
 app.use(express.static(new URL('../static', import.meta.url).pathname));
 
