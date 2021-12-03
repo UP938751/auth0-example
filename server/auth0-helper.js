8695b92513ee33266b27d887e9475495afd8c89c
UP938751 <40968375+UP938751@users.noreply.github.com>
2021-12-03 02:58:23 +0000

    stage 6

--- server/auth0-helper.js
+++ server/auth0-helper.js
@@ -9,3 +9,55 @@ const status401Errors = [
     'UnauthorizedError',
     'InvalidTokenError',
   ];
+
+export default function setup(authConfig) {
+  const checker = OAuth2JWTBearer.auth({
+    audience: authConfig.audience,
+    issuerBaseURL: `https://${authConfig.domain}`,
+  });
+
+  return {
+    getUserID,
+    checkJwt,
+    getProfile,
+  };
+
+  function getUserID(req) {
+    if (!req.auth || !req.auth.payload) return null;
+
+    // this is where OAuth2JWTBearer puts the user ID:
+    return req.auth.payload.sub;
+  }
+
+  // use OAuth2JWTBearer to check the actual token, but handle 401 errors
+  function checkJwt(req, res, next) {
+    return checker(req, res, (err) => {
+      if (err && status401Errors.includes(err.name)) {
+        res.sendStatus(401);
+      } else {
+        next(err);
+      }
+    });
+  }
+
+  async function getProfile(req) {
+    // if we don't have any auth information, there will be no profile
+    if (!req.auth || !req.auth.token) return null;
+
+    try {
+      const response = await fetch(`https://${authConfig.domain}/userinfo`, {
+        method: 'GET',
+        headers: {
+          authorization: `Bearer ${req.auth.token}`,
+        },
+      });
+
+      if (!response.ok) throw response;
+
+      return await response.json();
+    } catch (err) {
+      console.error('error getting auth profile', req.auth, err);
+      return null;
+    }
+  }
+}
--- server/server.js
+++ server/server.js
@@ -2,20 +2,35 @@ import express from 'express';
 
 import authConfig from './auth-config.js';
 
+import auth0Helpers from './auth0-helper.js';
+
 const app = express();
+
 
 // Serves the authentication configuration file to the client-side app
 app.get('/auth-config', (req, res) => {
   res.json(authConfig);
 });
 
-// a simple API endpoint (route) that will greet anyone
-app.get('/api/hello', (req, res) => {
-  res.send(`Hello! The time is ${new Date()}`);
+const auth0 = auth0Helpers(authConfig);
+
+// protect /api from unauthenticated users
+app.use('/api', auth0.checkJwt);
+
+app.get('/api/hello', async (req, res) => {
+  const userId = auth0.getUserID(req);
+
+  // load the user information, in production this would need caching or storing in a database
+  const profile = await auth0.getProfile(req);
+
+  res.send(`Hello user ${userId}, here's your profile:\n${JSON.stringify(profile, null, 2)}`);
+
+  console.log('successful authenticated request by ' + userId);
 });
 
 // this will serve the files present in public/ for all other requests
-app.use(express.static(new URL('../public', import.meta.url).pathname));
+// app.use(express.static(new URL('../public', import.meta.url).pathname));
+app.use(express.static('public'));
 
 // start the server
 const PORT = process.env.PORT || 8080;
